f43633795b0d17fcaef75c3f6699b54c8919ae72
OphaTapioka <ophashias@gmail.com>
2023-12-01 17:49:26 +0800

    Update GS2UnityDemoScript.cs

--- Assets/Scripts/Plugins/GS2Unity/Example/GS2UnityDemoScript.cs
+++ Assets/Scripts/Plugins/GS2Unity/Example/GS2UnityDemoScript.cs
@@ -5,10 +5,18 @@ using UnityEngine.UI;
 using TMPro;
 using UCharts;
 using System;
+using System.Linq;
 
 public class GS2UnityDemoScript : MonoBehaviour
 {
     public int[][] onlineTotalChoiceTallies;
+
+    public int[][] allVoteScoresNotSorted;
+    public int[] allVoteScoresSorted; 
+    
+    public int[][] topChoiceAndVotes; // top choices 1 - 5 and each one has 3 spots, 0-school 1-choiceNumber 2-voteScore
+
+    public int[][] bottomChoiceAndVotes; // bottom choices 1 - 5 and each one has 3 spots, 0-school 1-choiceNumber 2-voteScore    
     
     public TextMeshProUGUI topChoice1;
     public TextMeshProUGUI topChoice2;
@@ -88,11 +96,35 @@ public class GS2UnityDemoScript : MonoBehaviour
 
     public void Start()
     {
+        onlineTotalChoiceTallies = new int[5][];
         onlineTotalChoiceTallies[0] = new int[9]{0,0,0,0,0,0,0,0,0};
         onlineTotalChoiceTallies[1] = new int[10]{0,0,0,0,0,0,0,0,0,0};
         onlineTotalChoiceTallies[2] = new int[9]{0,0,0,0,0,0,0,0,0};
         onlineTotalChoiceTallies[3] = new int[18]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
         onlineTotalChoiceTallies[4] = new int[9]{0,0,0,0,0,0,0,0,0};
+
+        topChoiceAndVotes = new int[5][];
+        topChoiceAndVotes[0] = new int[3]{0,0,0}; 
+        topChoiceAndVotes[1] = new int[3]{0,0,0};
+        topChoiceAndVotes[2] = new int[3]{0,0,0};
+        topChoiceAndVotes[3] = new int[3]{0,0,0};
+        topChoiceAndVotes[4] = new int[3]{0,0,0};
+
+        bottomChoiceAndVotes = new int[5][];
+        bottomChoiceAndVotes[0] = new int[3]{0,0,0}; // 0-ethicalSchool 1-choiceNumber 2-voteScore
+        bottomChoiceAndVotes[1] = new int[3]{0,0,0};
+        bottomChoiceAndVotes[2] = new int[3]{0,0,0};
+        bottomChoiceAndVotes[3] = new int[3]{0,0,0};
+        bottomChoiceAndVotes[4] = new int[3]{0,0,0};
+
+        allVoteScoresNotSorted  = new int[55][];
+
+        for (int i = 0; i <= 54; i++)
+        {
+            allVoteScoresNotSorted[i]  = new int[3]{0,0,0};
+        }      
+        
+        allVoteScoresSorted     = new int[55];
         
         timeTextA.text = "x";
         rTextA.text =  "x";
@@ -121,8 +153,8 @@ public class GS2UnityDemoScript : MonoBehaviour
         radarA = GameObject.Find("RadarChart A");
         radarB = GameObject.Find("RadarChart B");
 
-        resultANumber = -1;
-        resultBNumber = -1;
+        resultANumber = 0;
+        resultBNumber = 0;
 
         resultsA = new string[10];
         resultsB = new string[10];
@@ -150,23 +182,158 @@ public class GS2UnityDemoScript : MonoBehaviour
 
     public void TallyBestAndWorstChoices()
     {
-        // 0-Uti. 1-Raw. 2-Vir.Eth. 3-NeoLib 4-Kant // Choices 1-16 each // jagged array that holds tally of how many times each choice is chosen
-        CountUtilitarian();
-        CountRawlsian();
-        CountVirtueEthics();
-        CountNeoliberal();
-        CountKantian();
+        // 0-Uti. 1-Raw. 2-Vir.Eth. 3-NeoLib 4-Kant // Choices 106 each // jagged array that holds tally of how many times each choice is chosen
+        AddUpChoiceTallies();   
+        PlaceAllScoresInTwoArrays();
+        IfThereAreDuplicatesMinus_iplusplus_ToAllScores();                    
+        /*
+        for (int i = 0; i < 4; i++)
+        {
+            if(onlineTotalChoiceTallies[i].Max() > topChoiceAndVotes[0][2])
+            {
+                topChoiceAndVotes[0][2] = onlineTotalChoiceTallies[i].Max();
+                //choiceNumber
+                topChoiceAndVotes[0][1] = Array.IndexOf(onlineTotalChoiceTallies[i],onlineTotalChoiceTallies[i].Max());
+                //school
+                topChoiceAndVotes[0][0] = i;
+            }
+        }
+        */              
     }
 
-    CountUtilitarian()
+    public void AddUpChoiceTallies()
     {
+        for (int i = 1; i < 1000; i++)
+        {
+            if(int.TryParse(txtSheet.GetRowData(i, "u0"), out _))
+            {
+                onlineTotalChoiceTallies[0][0] += int.Parse(txtSheet.GetRowData(i, "u0"));
+                onlineTotalChoiceTallies[0][1] += int.Parse(txtSheet.GetRowData(i, "u1"));
+                onlineTotalChoiceTallies[0][2] += int.Parse(txtSheet.GetRowData(i, "u2"));
+                onlineTotalChoiceTallies[0][3] += int.Parse(txtSheet.GetRowData(i, "u3"));
+                onlineTotalChoiceTallies[0][4] += int.Parse(txtSheet.GetRowData(i, "u4"));
+                onlineTotalChoiceTallies[0][5] += int.Parse(txtSheet.GetRowData(i, "u5"));
+                onlineTotalChoiceTallies[0][6] += int.Parse(txtSheet.GetRowData(i, "u6"));
+                onlineTotalChoiceTallies[0][7] += int.Parse(txtSheet.GetRowData(i, "u7"));
+                onlineTotalChoiceTallies[0][8] += int.Parse(txtSheet.GetRowData(i, "u8"));
+
+                onlineTotalChoiceTallies[1][0] += int.Parse(txtSheet.GetRowData(i, "r0"));
+                onlineTotalChoiceTallies[1][1] += int.Parse(txtSheet.GetRowData(i, "r1"));
+                onlineTotalChoiceTallies[1][2] += int.Parse(txtSheet.GetRowData(i, "r2"));
+                onlineTotalChoiceTallies[1][3] += int.Parse(txtSheet.GetRowData(i, "r3"));
+                onlineTotalChoiceTallies[1][4] += int.Parse(txtSheet.GetRowData(i, "r4"));
+                onlineTotalChoiceTallies[1][5] += int.Parse(txtSheet.GetRowData(i, "r5"));
+                onlineTotalChoiceTallies[1][6] += int.Parse(txtSheet.GetRowData(i, "r6"));
+                onlineTotalChoiceTallies[1][7] += int.Parse(txtSheet.GetRowData(i, "r7"));
+                onlineTotalChoiceTallies[1][8] += int.Parse(txtSheet.GetRowData(i, "r8"));
+                onlineTotalChoiceTallies[1][9] += int.Parse(txtSheet.GetRowData(i, "r9"));
+
+                onlineTotalChoiceTallies[2][0] += int.Parse(txtSheet.GetRowData(i, "ve0"));
+                onlineTotalChoiceTallies[2][1] += int.Parse(txtSheet.GetRowData(i, "ve1"));
+                onlineTotalChoiceTallies[2][2] += int.Parse(txtSheet.GetRowData(i, "ve2"));
+                onlineTotalChoiceTallies[2][3] += int.Parse(txtSheet.GetRowData(i, "ve3"));
+                onlineTotalChoiceTallies[2][4] += int.Parse(txtSheet.GetRowData(i, "ve4"));
+                onlineTotalChoiceTallies[2][5] += int.Parse(txtSheet.GetRowData(i, "ve5"));
+                onlineTotalChoiceTallies[2][6] += int.Parse(txtSheet.GetRowData(i, "ve6"));
+                onlineTotalChoiceTallies[2][7] += int.Parse(txtSheet.GetRowData(i, "ve7"));
+                onlineTotalChoiceTallies[2][8] += int.Parse(txtSheet.GetRowData(i, "ve8"));
+
+                onlineTotalChoiceTallies[3][0]  += int.Parse(txtSheet.GetRowData(i, "nl0"));
+                onlineTotalChoiceTallies[3][1]  += int.Parse(txtSheet.GetRowData(i, "nl1"));
+                onlineTotalChoiceTallies[3][2]  += int.Parse(txtSheet.GetRowData(i, "nl2"));
+                onlineTotalChoiceTallies[3][3]  += int.Parse(txtSheet.GetRowData(i, "nl3"));
+                onlineTotalChoiceTallies[3][4]  += int.Parse(txtSheet.GetRowData(i, "nl4"));
+                onlineTotalChoiceTallies[3][5]  += int.Parse(txtSheet.GetRowData(i, "nl5"));
+                onlineTotalChoiceTallies[3][6]  += int.Parse(txtSheet.GetRowData(i, "nl6"));
+                onlineTotalChoiceTallies[3][7]  += int.Parse(txtSheet.GetRowData(i, "nl7"));
+                onlineTotalChoiceTallies[3][8]  += int.Parse(txtSheet.GetRowData(i, "nl8"));
+                onlineTotalChoiceTallies[3][9]  += int.Parse(txtSheet.GetRowData(i, "nl9"));
+                onlineTotalChoiceTallies[3][10] += int.Parse(txtSheet.GetRowData(i, "nl10"));
+                onlineTotalChoiceTallies[3][11] += int.Parse(txtSheet.GetRowData(i, "nl11"));
+                onlineTotalChoiceTallies[3][12] += int.Parse(txtSheet.GetRowData(i, "nl12"));
+                onlineTotalChoiceTallies[3][13] += int.Parse(txtSheet.GetRowData(i, "nl13"));
+                onlineTotalChoiceTallies[3][14] += int.Parse(txtSheet.GetRowData(i, "nl14"));
+                onlineTotalChoiceTallies[3][15] += int.Parse(txtSheet.GetRowData(i, "nl15"));
+                onlineTotalChoiceTallies[3][16] += int.Parse(txtSheet.GetRowData(i, "nl16"));
+                onlineTotalChoiceTallies[3][17] += int.Parse(txtSheet.GetRowData(i, "nl17"));
+
+                onlineTotalChoiceTallies[4][0]  += int.Parse(txtSheet.GetRowData(i, "k0"));
+                onlineTotalChoiceTallies[4][1]  += int.Parse(txtSheet.GetRowData(i, "k1"));
+                onlineTotalChoiceTallies[4][2]  += int.Parse(txtSheet.GetRowData(i, "k2"));
+                onlineTotalChoiceTallies[4][3]  += int.Parse(txtSheet.GetRowData(i, "k3"));
+                onlineTotalChoiceTallies[4][4]  += int.Parse(txtSheet.GetRowData(i, "k4"));
+                onlineTotalChoiceTallies[4][5]  += int.Parse(txtSheet.GetRowData(i, "k5"));
+                onlineTotalChoiceTallies[4][6]  += int.Parse(txtSheet.GetRowData(i, "k6"));
+                onlineTotalChoiceTallies[4][7]  += int.Parse(txtSheet.GetRowData(i, "k7"));
+                onlineTotalChoiceTallies[4][8]  += int.Parse(txtSheet.GetRowData(i, "k8"));                
+            }
+        }
+    }
+
+    public void PlaceAllScoresInTwoArrays() //one sorted and one not sorted
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+            allVoteScoresNotSorted[i][2] = onlineTotalChoiceTallies[0][i];
+            allVoteScoresNotSorted[i][1] = i;
+            allVoteScoresNotSorted[i][0] = 0;
+        } 
+
+        for (int i = 9; i <= 18; i++)
+        {
+            allVoteScoresNotSorted[i][2] = onlineTotalChoiceTallies[1][i-9];
+            allVoteScoresNotSorted[i][1] = i-9;
+            allVoteScoresNotSorted[i][0] = 1;
+        }    
+
+        for (int i = 19; i <= 27; i++)
+        {
+            allVoteScoresNotSorted[i][2] = onlineTotalChoiceTallies[2][i-19];
+            allVoteScoresNotSorted[i][1] = i-19;
+            allVoteScoresNotSorted[i][0] = 2;
+        }
+
+        for (int i = 28; i <= 45; i++)
+        {
+            allVoteScoresNotSorted[i][2] = onlineTotalChoiceTallies[3][i-28];
+            allVoteScoresNotSorted[i][1] = i-28;
+            allVoteScoresNotSorted[i][0] = 3;
+        }
+
+        for (int i = 46; i <= 54; i++)
+        {
+            allVoteScoresNotSorted[i][2] = onlineTotalChoiceTallies[4][i-46];
+            allVoteScoresNotSorted[i][1] = i-46;
+            allVoteScoresNotSorted[i][0] = 4;
+        }  
+        /*
+        for (int i = 0; i <= 54; i++)
+        {
+            allVoteScoresSorted[i] = allVoteScoresNotSorted[i][0];
+        }  
+        */
+        //var columnIdx = 0;
+        //Array.Sort(allVoteScoresNotSorted, (x, y) => x[columnIdx].CompareTo(y[columnIdx]));
         
+        for (int i = 0; i <= 54; i++)
+        {
+            Debug.Log("Choice Place " + i.ToString() + " is school " + allVoteScoresNotSorted[i][0].ToString() + ",    choice " + allVoteScoresNotSorted[i][1].ToString() + ",  score " + allVoteScoresNotSorted[i][2].ToString());
+        }
+                   
+    }
+
+    public void IfThereAreDuplicatesMinus_iplusplus_ToAllScores()
+    {
+        if (allVoteScoresSorted.Length != allVoteScoresSorted.Distinct().Count())
+        {
+            
+        }   
     }
     
     public void searchNameResults(string typedName)
     {
-        resultANumber = -1;
-        resultBNumber = -1;
+        resultANumber = 0;
+        resultBNumber = 0;
         ClearResultsAandB();
 
         timeTextA.text = "x";
@@ -235,7 +402,7 @@ public class GS2UnityDemoScript : MonoBehaviour
 
     public void SelectionAIncrease()
     {
-        if (resultANumber >= currentResultNumber -1)
+        if (resultANumber >= currentResultNumber - 1)
         {
             resultANumber = 0;
             ChangeResultsA(resultANumber);
